c02d7a59730ad0921c57f05df3ea01b5527fb884
Aaron Smith <aaroncosmith@gmail.com>
2020-04-01 12:20:09 -0400

    finished event handlers, forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    textInput: "",
+    userInput: []
+  };
+
+  changeHandler = event => {
+    this.setState({
+      textInput: event.target.value
+    });
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    const previousInput = this.state.textInput;
+    this.setState({
+      textInput: "",
+      userInput: [...this.state.userInput, previousInput]
+    });
+  };
+
   render () {
     return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={this.submitHandler}>
+        <label>
+          Github Profile Picture Finder
+          <input
+            type="text"
+            value={this.state.textInput}
+            placeholder="Enter Profile Name"
+            onChange={this.changeHandler}
+          />
+        </label>
+        <button type="submit">SEND IT!</button>
+      </form>
+    <p>{this.state.textInput}</p>
     </div>
   );}
 }
